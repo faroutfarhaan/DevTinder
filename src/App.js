736bb7cafbe4446c67dea4578e39fc93d0830cc6
faroutfarhaan <133531252+faroutfarhaan@users.noreply.github.com>
2025-03-22 16:25:40 +0530

    created Api's

--- src/App.js
+++ src/App.js
@@ -2,23 +2,71 @@ const express = require("express");
 const app =express();
 const {connectDB}=require("./config/database.js");
 const User=require("./config/models/user.js");
+app.use(express.json());
+// Get user using email API
+app.get("/user", async (req,res)=>{
+    const userEmail=req.body.email;
+    try{
+        const users=await User.findOne({email:userEmail});
+        if(users.length===0){
+            res.status(404).send("User not found");
+        }else{
+            res.send(users);
+        }
+    }
+    catch (err){
+        res.status(400).send("Something went wrong");
+    }
+});
+// Feed Api
+app.get("/feed", async (req,res)=>{
+    try{
+        const users=await User.find();
+        if(users.length===0){
+            res.status(404).send("No users found");
+        }else{
+            res.send(users);
+        }
+    }
+    catch (err){
+        res.status(400).send("Something went wrong");
+    }
+});
+// delete api
+app.delete("/user", async (req,res)=>{
+    const userId=req.body.userId;
+    try{
+        await User.findByIdAndDelete(userId);
+        res.send("user deleted successfully");
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+    }
 
-app.post("/signup",async (rq,res)=>{
-    try{ const user=new User({
-        firstName:"Moh Farhan",
-        lastName:"Patel",
-        email:"mohfarhanpatel@gmail.com",
-        phone:"123456789",
-        age:"22",
-        password:"abcdefgh"
 
-    });
+});
+// update api
+app.patch("/user", async (req,res)=>{
+    const userId=req.body.userId;
+    const data=req.body;
+    try{
+      await User.findByIdAndUpdate({_id:userId},data,{runValidations:true});
+      res.send("User updated");
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+    }
+});
+// SignUp Api
+app.post("/signup",async (req,res)=>{
+    try{ const user=new User(req.body);
     await user.save();
     res.send("User Signed Up!");}
     catch(err){
         console.log(err);
     };
 });
+// connecting to DB
 connectDB()
 .then( ()=>{
     console.log("DB connected");
@@ -29,5 +77,3 @@ connectDB()
 .catch((err)=>{
     console.log("My Lord, sorry to inform you but DB has not connected");
 });
-
-
--- src/config/models/user.js
+++ src/config/models/user.js
@@ -2,26 +2,46 @@ const mongoose= require("mongoose");
 const{Schema}=require("mongoose");
 const userSchema=new Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        trim:true
     },
     lastName:{
         type:String
     },
     email:{
-        type:String
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true
     },
     age:{
-        type:String
+        type:String,
+        min:15
     },
     phone:{
-        type:String
+        type:String,
+        unique:true,
+        minLength:10,
+        maxlength:10
     },
     gender:{
-        type:String
+        type:String,
+        enum:["male","female","others"],
+        validator(value){
+            if(!["male","female","others"].includes(value)){
+                throw new err("Gender data is invalid")
+            }
+        }
     },
     password:{
-        type:String
+        type:String,
+        minLength:8,
+        required:true
     }
+},{
+    timestamps:true
 })
 const User = mongoose.model("User",userSchema);
 module.exports=User;
